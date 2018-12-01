89da5facfab1754f0864b01b30d48618033fdd53
Chandan Kumar <chandanchaudhary@outlook.com>
2018-12-02 02:10:30 +0530

    Add port present in env for running express app

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const socket = require('socket.io');
 const app = express();
 const connections = {};
 
-const server = app.listen(4000, () => {
-  console.log('running on port 4000');
-});
+const server = app.listen(process.env.PORT || 4000);
 
 const io = socket(server);
 
